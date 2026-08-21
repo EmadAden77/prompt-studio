f30dcf762deec6594ea12e66ee5a393b392a5e94
EmadAden77 <sasfsas8@gmail.com>
2026-08-21 16:06:08 +0300

    Delete Prompt Studio project files

--- version-fix.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  function mark(){
-    var b=document.querySelector('.badge');
-    if(b)b.textContent=(location.hash&&/^(#car|#bedroom|#places)$/.test(location.hash))?'v3.72':'Browser v3.72';
-    var m=document.querySelector('.meta span:last-child');
-    if(m)m.textContent='Prompt Studio Browser v3.72';
-  }
-  if(document.readyState==='loading')document.addEventListener('DOMContentLoaded',function(){setTimeout(mark,450)});else setTimeout(mark,450);
-})();
