a8a4bb4d51b07578de548d36d5a8bc5284529766
EmadAden77 <sasfsas8@gmail.com>
2026-09-06 16:16:41 +0300

    Phase 40: add street section module

---
+++ js/sections/street.js
@@ -0,0 +1,3 @@
+import { deepFreeze } from "./_freeze.js";
+export const SECTION = deepFreeze({ id:"street", label:"Street", description:"Saudi street standing and walking selfie", captureType:"direct_front_camera_selfie", scenes:["street"], clothingSource:"authority", poses:["standing","walking"], lighting:["outdoor"], realismLayers:["saudi-street","saudi-realism","micro"], rules:{ hard:["outdoor street context"], composition:["standing or walking"], interaction:[], exclusions:["studio lighting"] } });
+export default SECTION;
