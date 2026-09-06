467ef3fd39de914612a613d7269099565b372d93
EmadAden77 <sasfsas8@gmail.com>
2026-09-06 16:19:34 +0300

    Phase 40: add custom section module

---
+++ js/sections/custom.js
@@ -0,0 +1,21 @@
+import { deepFreeze } from "./_freeze.js";
+
+export const SECTION = deepFreeze({
+  id:"custom",
+  label:"Custom",
+  description:"User-written free scene",
+  captureType:"direct_front_camera_selfie",
+  scenes:["custom"],
+  clothingSource:"custom",
+  poses:["free"],
+  lighting:["contextual"],
+  realismLayers:["micro"],
+  rules:{
+    hard:["preserve user-written place"],
+    composition:["free composition"],
+    interaction:[],
+    exclusions:[]
+  }
+});
+
+export default SECTION;
