1ef97b7db2bfe5ec93d5a3a11e7170a3e92a8f78
EmadAden77 <sasfsas8@gmail.com>
2026-09-06 16:18:17 +0300

    Phase 40: add section deep-freeze helper

---
+++ js/sections/_freeze.js
@@ -0,0 +1,7 @@
+export function deepFreeze(value) {
+  if (!value || typeof value !== "object" || Object.isFrozen(value)) return value;
+  for (const child of Object.values(value)) deepFreeze(child);
+  return Object.freeze(value);
+}
+
+export default deepFreeze;
