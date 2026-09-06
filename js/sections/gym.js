6b94f51329912ce31f1b5195a6c4c0606a522295
EmadAden77 <sasfsas8@gmail.com>
2026-09-06 16:16:35 +0300

    Phase 40: add gym section module

---
+++ js/sections/gym.js
@@ -0,0 +1,3 @@
+import { deepFreeze } from "./_freeze.js";
+export const SECTION = deepFreeze({ id:"gym", label:"Gym", description:"Natural gym selfie and fitness context", captureType:"direct_front_camera_selfie", scenes:["gym"], clothingSource:"gym", poses:["gym-selfie","post-workout"], lighting:["gym"], realismLayers:["gym","micro"], rules:{ hard:["gym context"], composition:["fitness-appropriate pose"], interaction:[], exclusions:["luxury accessories"] } });
+export default SECTION;
