8aa4c6a963ad000ab7e70b3eff3ad63f209a0eed
EmadAden77 <sasfsas8@gmail.com>
2026-09-06 16:16:24 +0300

    Phase 40: add car exterior section module

---
+++ js/sections/carExterior.js
@@ -0,0 +1,3 @@
+import { deepFreeze } from "./_freeze.js";
+export const SECTION = deepFreeze({ id:"carExterior", label:"Car Exterior", description:"Selfie beside the locked 2017 Range Rover", captureType:"direct_front_camera_selfie", scenes:["carExterior"], clothingSource:"authority", poses:["exterior-authority"], lighting:["exterior-authority"], realismLayers:["car-spec-lock","glass","contact-shadow"], rules:{ hard:["2017 Range Rover Sport Autobiography Dynamic L494","Fuji White","exterior only"], composition:["location and pose authority"], interaction:["beside vehicle"], exclusions:["driving"] } });
+export default SECTION;
