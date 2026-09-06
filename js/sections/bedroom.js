1517298cc8386f3dad31a072bb1fbcea55c670af
EmadAden77 <sasfsas8@gmail.com>
2026-09-06 16:16:30 +0300

    Phase 40: add bedroom section module

---
+++ js/sections/bedroom.js
@@ -0,0 +1,3 @@
+import { deepFreeze } from "./_freeze.js";
+export const SECTION = deepFreeze({ id:"bedroom", label:"Bedroom", description:"Lived-in bedroom selfie and home poses", captureType:"direct_front_camera_selfie", scenes:["bedroom"], clothingSource:"home", poses:["bed","sofa","lying","selfie"], lighting:["room"], realismLayers:["lived-in","imperfections"], rules:{ hard:["bedroom context"], composition:["bed sofa or lying pose"], interaction:[], exclusions:["studio staging"] } });
+export default SECTION;
