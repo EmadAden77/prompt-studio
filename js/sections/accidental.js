4a3d4e504e72810f8b6be51867213e2b7e15441a
EmadAden77 <sasfsas8@gmail.com>
2026-09-06 16:16:46 +0300

    Phase 40: add accidental section module

---
+++ js/sections/accidental.js
@@ -0,0 +1,3 @@
+import { deepFreeze } from "./_freeze.js";
+export const SECTION = deepFreeze({ id:"accidental", label:"Accidental", description:"Candid accidental front-camera capture", captureType:"accidental_front_camera_capture", scenes:["street"], clothingSource:"authority", poses:["candid-motion"], lighting:["available"], realismLayers:["camera-accident","micro"], rules:{ hard:["candid capture"], composition:["phone motion focus and exposure accidents"], interaction:[], exclusions:["posed selfie"] } });
+export default SECTION;
