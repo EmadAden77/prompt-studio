592735a57a3ae5ceabece8bac9010f5113e8f50a
EmadAden77 <sasfsas8@gmail.com>
2026-09-06 23:53:45 +0300

    Phase 53: switch live engine gate

--- js/canonical/engine-gate.js
+++ js/canonical/engine-gate.js
@@ -1,6 +1,6 @@
 import { wikiPromptService } from "../services/wikiPromptService.js";
 import { STREET_MOODS } from "../data.js";
-import { buildCanonicalV3UserOutput } from "./canonical-v3-phase52-1.js";
+import { buildCanonicalV3UserOutput } from "./canonical-v3-phase53.js";
 import {
   CANONICAL_V3_ENGINE,
   ENGINE_STORAGE_KEY,
