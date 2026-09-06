29b99da101e676b8daa303abcb7300f3676a3522
EmadAden77 <sasfsas8@gmail.com>
2026-09-06 16:16:13 +0300

    Phase 40: add group section module

---
+++ js/sections/group.js
@@ -0,0 +1,3 @@
+import { deepFreeze } from "./_freeze.js";
+export const SECTION = deepFreeze({ id:"group", label:"Group", description:"Two-to-six-person phone-held group selfie", captureType:"group_selfie", scenes:["street"], clothingSource:"authority", poses:["phone-holder","distributed-group"], lighting:["group-natural"], realismLayers:["group-diversity","soft-life"], rules:{ hard:["2-6 people","one phone-holder"], composition:["natural subject distribution","scene picker"], interaction:["distinct group identities"], exclusions:[] } });
+export default SECTION;
